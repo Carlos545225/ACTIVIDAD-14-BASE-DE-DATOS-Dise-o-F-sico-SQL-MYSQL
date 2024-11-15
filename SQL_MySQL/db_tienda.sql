1dcc786f5eda8eb81a5a88d52185dbca8ccddc44
Carlos545225 <115045524+Carlos545225@users.noreply.github.com>
2024-11-14 19:46:29 -0500

    Add files via upload

---
+++ SQL_MySQL/db_tienda.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS db_tienda;
+USE db_tienda;
+
+CREATE TABLE empleados (
+    idEmpleado INT PRIMARY KEY AUTO_INCREMENT,
+    nombreCompleto VARCHAR(100) NOT NULL,
+    fechaNacimiento DATE NOT NULL,
+    fechaContratacion DATE DEFAULT CURRENT_DATE,
+    puesto VARCHAR(50) NOT NULL,
+    salario DECIMAL(10, 2) NOT NULL,
+    email VARCHAR(100) UNIQUE
+);
+
